428e664327b411bde537effba0e76c9a963c4273
johntyler3 <johntyler.thomas3@gmail.com>
2020-08-09 22:53:43 -0500

    working on several probs

---
+++ src/main/java/leetCode/JewelsAndStones.java
@@ -0,0 +1,18 @@
+package leetCode;
+
+public class JewelsAndStones {
+
+    public int numJewelsInStones(String J, String S) {
+
+        int count = 0;
+
+        for (char ch : S.toCharArray()) {
+            if (J.indexOf(ch) >= 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
