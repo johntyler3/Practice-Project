a0cc10b82c86895514c50ab18c7728a9cba02df5
johntyler3 <johntyler.thomas3@gmail.com>
2020-08-24 22:24:49 -0500

    subtractProduct and sum

---
+++ src/main/java/leetCode/SubtractProductAndSum.java
@@ -0,0 +1,23 @@
+package leetCode;
+
+public class SubtractProductAndSum {
+
+    public int subtractProductAndSum(int n) {
+        int productDigits = 1;
+        if (n == 0) {
+            productDigits = 0;
+        }
+
+        int sumDigits = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            productDigits *= digit;
+            sumDigits += digit;
+            n /= 10;
+        }
+
+        return productDigits - sumDigits;
+    }
+
+}
