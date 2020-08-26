e9cfef7e4f9cefa637aca2a39df8f656af98e0b3
johntyler3 <johntyler.thomas3@gmail.com>
2020-08-25 22:13:07 -0500

    dec

---
+++ src/main/java/leetCode/DecompressRLElist.java
@@ -0,0 +1,32 @@
+package leetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DecompressRLElist {
+
+    public int[] decompressRLElist(int[] nums) {
+
+        ArrayList<Integer> myList = new ArrayList();
+
+        for (int i = 0; i < nums.length; i +=2) {
+            int addElement = nums[i + 1];
+            for (int j = 0; j < nums[i]; j++) {
+                myList.add(addElement);
+            }
+        }
+
+        return getArray(myList);
+    }
+
+    public int[] getArray(List<Integer> list) {
+        int[] returnArray = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            returnArray[i] = list.get(i);
+        }
+
+        return returnArray;
+    }
+
+}
